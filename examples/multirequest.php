5f434d38253a6e2190fe84b0f591c83f4a46eb01
Steve Lacey <steve@stevelacey.net>
2013-12-18 22:08:47 +0000

    Adjusted namespaces and implemented DAV client from fruux/sabre-dab.

--- examples/multirequest.php
+++ examples/multirequest.php
@@ -2,7 +2,7 @@
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$client = new Sabre\DAVClient\Client();
+$client = new Sabre\DAV\Client;
 
 $urls = [
     'http://www.google.com/',
@@ -22,28 +22,24 @@ $urls = [
 
 ];
 
-foreach($urls as $url) {
-
+foreach ($urls as $url) {
     $request = new \Sabre\HTTP\Request('GET', $url);
+
     echo "add $url\n";
 
     $client->addToRequestQueue(
         $request,
-        function(\Sabre\HTTP\ResponseInterface $response) use ($url) {
-
+        function (\Sabre\HTTP\ResponseInterface $response) use ($url) {
             echo $response->getStatus() . " $url\n";
-
         },
-        function($errorType, array $errorInfo) {
-
+        function ($errorType, array $errorInfo) {
             echo "error $url\n";
-
         }
     );
 
     // You can totally take out this sleep statement. It's just added to show
     // that this is truly asynchronous.
     usleep(500000); // 200ms
-
 }
+
 $client->wait();
