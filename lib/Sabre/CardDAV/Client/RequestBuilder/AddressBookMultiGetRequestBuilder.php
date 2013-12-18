2d29f170f9f0061fc899d06d58bbf02d18a0f506
Steve Lacey <steve@stevelacey.net>
2013-12-18 22:09:25 +0000

    RequestBuilder concept + CardDAV AddressBookMultiGetRequestBuilder example.

---
+++ lib/Sabre/CardDAV/Client/RequestBuilder/AddressBookMultiGetRequestBuilder.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Sabre\CardDAV\Client\RequestBuilder;
+
+use Sabre\DAV\Client\RequestBuilder\RequestBuilderInterface;
+use Sabre\HTTP;
+use XMLWriter;
+
+class AddressBookMultiGetRequestBuilder implements RequestBuilderInterface
+{
+    protected $contacts;
+
+    protected $headers = ['Content-Type' => 'application/xml'];
+
+    protected $method = 'REPORT';
+
+    protected $url;
+
+    public function __construct($url, array $contacts)
+    {
+        $this->url = $url;
+        $this->contacts = $contacts;
+    }
+
+    public function build()
+    {
+        return new HTTP\Request($this->method, $this->url, $this->headers, $this->writeXML());
+    }
+
+    protected function writeXML()
+    {
+        $xml = new XMLWriter;
+        $xml->openMemory();
+        $xml->setIndent(4);
+        $xml->startDocument('1.0', 'utf-8');
+            $xml->startElement('a:addressbook-multiget');
+                $xml->writeAttribute('xmlns:d', 'DAV:');
+                $xml->writeAttribute('xmlns:a', 'urn:ietf:params:xml:ns:carddav');
+                $xml->startElement('d:prop');
+                    $xml->writeElement('d:getetag');
+                    $xml->writeElement('a:address-data');
+                $xml->endElement();
+
+                foreach ($this->contacts as $contact) {
+                    $xml->writeElement('d:href', $contact);
+                }
+
+            $xml->endElement();
+        $xml->endDocument();
+
+        return $xml->outputMemory();
+    }
+}
