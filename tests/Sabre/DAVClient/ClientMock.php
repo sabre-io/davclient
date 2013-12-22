4114e6e03d83569952cd87127191fc373a6a361b
Evert Pot <evert@rooftopsolutions.nl>
2013-12-22 05:22:02 +0100

    Remving a lot of tests that don't make a lot of sense anymore.

--- tests/Sabre/DAVClient/ClientMock.php
+++ tests/Sabre/DAVClient/ClientMock.php
@@ -7,20 +7,6 @@ class ClientMock extends Client {
     public $response;
 
     public $url;
-    public $curlSettings;
-
-    protected function curlRequest($curlSettings) {
-
-        // We're just doing this so we don't have to change the unittests that
-        // much ;)
-        $this->url = $curlSettings[CURLOPT_URL];
-
-        unset($curlSettings[CURLOPT_URL]);
-
-        $this->curlSettings = $curlSettings;
-        return $this->response;
-
-    }
 
     /**
      * Just making this method public
